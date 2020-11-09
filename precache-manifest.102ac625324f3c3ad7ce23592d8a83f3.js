ae26ea80925ab8371f38223f94254f3c416b73dd
AvroraPolnareff <araintheskywith@gmail.com>
2020-11-09 17:07:14 +0300

    Updates

---
+++ precache-manifest.102ac625324f3c3ad7ce23592d8a83f3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b86a375ebdc74723bfba678cc62db60",
+    "url": "/test-vk-mini-app/index.html"
+  },
+  {
+    "revision": "a7a7220bb1076244f712",
+    "url": "/test-vk-mini-app/static/css/2.8fde3e13.chunk.css"
+  },
+  {
+    "revision": "6c40814437614e2b8c7d",
+    "url": "/test-vk-mini-app/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "a7a7220bb1076244f712",
+    "url": "/test-vk-mini-app/static/js/2.f44219e8.chunk.js"
+  },
+  {
+    "revision": "33490309eac47256b827830c802fd4a5",
+    "url": "/test-vk-mini-app/static/js/2.f44219e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c40814437614e2b8c7d",
+    "url": "/test-vk-mini-app/static/js/main.f7bd676a.chunk.js"
+  },
+  {
+    "revision": "d7de075102c53e1728f7",
+    "url": "/test-vk-mini-app/static/js/runtime-main.c5c91f99.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/test-vk-mini-app/static/media/persik.4e1ec840.png"
+  }
+]);
